71e1b2244da388a70cd7b3af10c76ec8a285b0d0
Hugo-ctrl <alveshugo248@gmail.com>
2020-03-03 16:53:28 -0300

    Acrescentada uma mensagem de bom dia

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace MyTeste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Bom dia");
             Console.ReadLine();
         }
     }
